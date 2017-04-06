4919827d1a78ec8635b89e368f138d7d509f1aab
jesperengstrom <jengstro@gmail.com>
2017-04-06 18:18:22 +0200

    updated + setting obj literal with map

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,15 +1,22 @@
 var storage = (function() {
 
-    var repos = [{
+    const repos = [{
+            name: "Talartoppen",
+            date: "April 2017",
+            link: "https://jesperengstrom.github.io/Talartoppen/",
+            ghlink: "https://github.com/jesperengstrom/Talartoppen",
+            description: "An exersise in using async JS & Ajax. Ranks the politicians that debate the most in the Swedish parliament.",
+        },
+        {
             name: "JMDb",
-            date: "Mars 2017",
+            date: "March 2017",
             link: "https://jesperengstrom.github.io/JMDb/",
             ghlink: "https://github.com/jesperengstrom/JMDb",
             description: "An exersise in using JavsScript design patterns, closures and scope by building a mock movie database.",
         },
         {
             name: "Asap Development",
-            date: "Januari 2017",
+            date: "January 2017",
             link: "https://danteuh.github.io/asap-development/",
             ghlink: "https://github.com/DanteUh/asap-development/",
             description: "A collaborative exercise in HTML5, SASS & Bootstrap with the task to build a website for a fake web development agency.",
@@ -28,17 +35,10 @@ var storage = (function() {
             ghlink: "https://github.com/jesperengstrom/todo",
             description: "A small school exercise in using arrays and the DOM.",
         },
-        {
-            name: "Foodie",
-            date: "November 2016",
-            link: "https://jesperengstrom.github.io/foodie/",
-            ghlink: "https://github.com/jesperengstrom/foodie",
-            description: "A small school exercise in using objects, functions and the DOM.",
-        }
     ];
 
     return {
-        getAll: function() {
+        getAll: () => {
             return printItems(repos);
         }
     };
@@ -47,18 +47,17 @@ var storage = (function() {
 
 function printItems(items) {
     let main = document.getElementById("mainbox");
-    for (let item of items) {
-        main.innerHTML +=
-            `<p class="tooltip"><span class="tooltiptext">${item.description}</span>
-            <a class="title" href="${item.link}">${item.name}</a> 
-            <a class="repo" href="${item.ghlink}">[repo]</a> 
-            </span><span class="date">${item.date}</span></p>
-        `
 
-    }
+    const html = items.map(el => {
+        return `<p class="tooltip"><span class="tooltiptext">${el.description}</span>
+             <a class="title" href="${el.link}" target="_blank">${el.name}</a> 
+            <a class="repo" href="${el.ghlink}" target="_blank">[repo]</a> 
+             </span><span class="date">${el.date}</span></p>`;
+    }).join("");
+
+    main.innerHTML = html;
 
 }
-
 
 (function() {
     let start = window.addEventListener("DOMContentLoaded", storage.getAll());
